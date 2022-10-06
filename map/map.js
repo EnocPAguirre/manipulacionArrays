07467ac5da26077052e3f2006c2b5a70d5a99f87
EnocPAguirre <enocpinedaa@gmail.com>
2022-10-06 00:18:58 -0500

    map

---
+++ map/map.js
@@ -0,0 +1,6 @@
+//Recordar que es inmutable por lo tanto solo puede regresar un nuevo array pero//no modifica los originales
+
+const frutas = ['fresa', 'piña', 'papaya'];
+
+frutas.map((element) => element + "++");
+
